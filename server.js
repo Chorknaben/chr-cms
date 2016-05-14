a982297d5fd3f166dcec0853466209af9d417020
Georg Grab <ggrab@thereisnobreak.net>
2016-05-14 03:59:44 +0200

    initial backend commit

---
+++ server.js
@@ -0,0 +1,438 @@
+const http = require("http");
+const express = require("express");
+const bodyParser = require("body-parser");
+var randomstring = require("randomstring");
+var child_process = require("child_process");
+var busboy = require("connect-busboy")
+var backend = express();
+backend.use(busboy());
+var fs = require("fs");
+var ini = require("ini");
+
+var config = ini.parse(fs.readFileSync("./server.config", "utf-8"))
+console.log(config);
+
+function dieUnlessConfigSupplied($config, mandatory){
+	for (var i of mandatory){
+		var tempObject = $config;
+		for(var subObject of i.split(".")){
+			tempObject = tempObject[subObject]
+			if(tempObject === undefined){
+				console.error("Mandatory Option " + i + " fehlt. Abbruch");
+				process.exit(1)
+			}
+		}
+	}
+}
+dieUnlessConfigSupplied(config, 
+		["server.host", "server.port", 
+		"data.dataDirectory", "data.locations.calendar", "data.locations.newsticker", "data.locations.images"]);
+
+var kalender = express.Router();
+var newsticker = express.Router();
+var bilder = express.Router();
+
+var eventsJson;
+fs.readFile(__dirname + config.data.dataDirectory + config.data.locations.calendar , "utf8", function(err , data){
+	eventsJson = JSON.parse(data)
+});
+
+var newstickerJson;
+fs.readFile(__dirname + config.data.dataDirectory + config.data.locations.newsticker , "utf8", function(err , data){
+	newstickerJson = JSON.parse(data)
+});
+
+function writeEventsJson(){
+	// Save Changes
+	// Evtl. erst später speichern. also call auf writeFile erst bei klick auf einen "Deploy" - Button ausführen, oder so
+	fs.writeFile(__dirname + config.data.dataDirectory + config.locations.calendar, JSON.stringify(eventsJson), function(error){
+		if(error){
+			return console.error(error);
+		}
+		console.log("Schreibvorgang auf events.json erfolgreich ausgeführt.");
+	});
+}
+
+function writeNewstickerJson(){
+	fs.writeFile(__dirname + config.data.dataDirectory + config.locations.newsticker, JSON.stringify(newstickerJson), function(error){
+		if(error){
+			return console.error(error);
+		}
+		console.log("Schreibvorgang auf newsticker.json erfolgreich ausgeführt.");
+	});
+}
+
+
+kalender.get("/get", function(request, response){
+	// TODO echo KALENDER JSON FILE
+	console.info("In /kalender/get");
+
+	response.end(JSON.stringify(eventsJson));
+
+
+});
+
+var jsonParser = bodyParser.json();
+
+kalender.post("/add", jsonParser, function(request, response){
+	// TODO PARSE REQUEST BODY, VALIDATE, APPEND TO JSON FILE
+	// OR RETURN AN ERROR
+	console.info("In /kalender/add");
+	if (!request.body) return response.sendStatus(400);
+
+	var expectedKeys = ["date", "location", "time", "title"];
+	var dataReceived = request.body;
+
+	for (var i = expectedKeys.length - 1; i >= 0; i--) {
+		if (!dataReceived.hasOwnProperty(expectedKeys[i])){
+			response.end(JSON.stringify({
+				err: "Einer von date,location,time,title keys wurde nicht gefunden",
+				code: "kal_1_err"
+			}));
+			return;
+		}
+	};
+
+	for (var i = eventsJson["events"].length - 1; i >= 0; i--) {
+		var amountDuplicateKeys = 0;
+		for (var m = expectedKeys.length - 1; m >= 0; m--) {
+			if(dataReceived[expectedKeys[m]] == eventsJson["events"][i][expectedKeys[m]]){
+				amountDuplicateKeys++;
+
+			}
+		};
+		console.log("amountDuplicateKeys: " + amountDuplicateKeys);
+		if (amountDuplicateKeys == 4){
+			// exaktes duplikat
+			response.end(JSON.stringify({
+					err: "Duplikat mit bestehender Eventdatenbank erkannt. Vorgang wird abgebrochen.",
+					code: "kal_2_err"
+			}));
+			return;
+		}
+	};
+
+	eventsJson["events"].push(dataReceived);
+	writeEventsJson();
+	response.end(JSON.stringify(eventsJson));
+
+
+
+	// Expected POST Request Body is a calendar object
+	/*
+        {
+            "date": "07.12.2014",
+            "location": "Marktplatz Biberach (Ri&szlig;)",
+            "time": "19.00",
+            "title": "Biberacher Christkindlesmarkt"
+        }
+	*/
+
+
+
+});
+kalender.post("/rm", jsonParser, function(request, response){
+	// TODO PARSE REQUEST BODY, VALIDATE, REMOVE FROM JSON FILE
+	console.info("In /kalender/rm");
+	if (request.body["type"] == "byID"){
+		var deletedElement = eventsJson["events"].splice(request.body["object"], 1);
+		response.end(JSON.stringify(deletedElement));
+		writeEventsJson();
+		return;
+
+	} else if (request.body["type"] == "byObjType"){
+		// TODO Implement if needed
+		var object = request.body["object"];
+		for (var i = eventsJson["events"].length - 1; i >= 0; i--) {
+			if (eventsJson["events"][i].date === object.date &&
+				eventsJson["events"][i].time === object.time &&
+				eventsJson["events"][i].location === object.location &&
+				eventsJson["events"][i].title === object.title){
+				console.log("Element to delete found at idx: " + i);
+			var deletedElement = eventsJson["events"].splice(i, 1);
+			response.end(JSON.stringify(deletedElement));
+			writeEventsJson();
+			return;
+			}
+		};
+	} else {
+		response.end(JSON.stringify({
+			err: "angegebener Suchalgorithmus für JSON type key ist unbekannt. vorgang wird abgebrochen.",
+			code: "kal_3_err"
+		}));
+		return;
+	}
+
+});
+
+newsticker.get("/get", function(request, response){
+	console.log("In /newsticker/get");
+	response.end(JSON.stringify(newstickerJson));
+
+});
+newsticker.post("/set", jsonParser, function(request, response){
+	var dataReceived = request.body;
+	if(!dataReceived["text"]){
+		response.end(JSON.stringify({
+			err: "Kein Inhalt",
+			code: "news_1_err"
+		}));
+	} else{
+		console.log("Recieved:" + JSON.stringify(dataReceived));
+		newstickerJson = {news:dataReceived["text"]};
+		writeNewstickerJson();
+		response.end(JSON.stringify(newstickerJson));
+	}
+
+});
+
+var metaJson = {};
+function readMetaJson(){
+	try{
+	    metaJson = JSON.parse(fs.readFileSync(__dirname + config.struktur.bilder + "/" + "meta", "utf-8"));
+	} catch(error){
+		console.warn(error);
+	}
+    return metaJson;
+}
+function writeMetaJson(prod){
+    metaJson["prod"] = prod;
+    fs.writeFileSync(__dirname + config.struktur.bilder + "/meta", JSON.stringify(metaJson), 'utf8');
+    console.log("meta write ok");
+    setProduction();
+}
+
+function setProduction(){
+	readMetaJson();
+	console.log("Production: ln -sr bilderStrukturen/" + metaJson.prod + " dataSymlink/image");
+	child_process.spawnSync('rm', ['dataSymlink/image', 'dataSymlink/json/bilder.json']);
+	child_process.spawn('ln', ['-sr', 'bilderStrukturen/' + metaJson.prod, 'dataSymlink/image']); 
+	child_process.spawn('ln', ['-sr', 'bilderStrukturen/' + metaJson.prod + ".json", 'dataSymlink/json/bilder.json']); 
+}
+
+var allStructures;
+function getStructures(){
+	allStructures = fs.readdirSync(__dirname + config.struktur.bilder);
+	allStructures = allStructures.filter(function(val){
+		return val.indexOf("_") == -1 && fs.statSync(__dirname + config.struktur.bilder + "/" + val).isDirectory()
+	})
+	return allStructures;
+
+}
+bilder.get("/strukturen", function(request, response){
+	// Read directories in struktur Directory
+	getStructures();
+	// Read Metadata File
+	var metaJSON = readMetaJson();
+
+
+	// Merge and repond
+	response.end(JSON.stringify({
+		"meta" : metaJSON,
+		"strukturen" : allStructures
+	}));
+});
+
+bilder.post("/upload", function(request, response){
+	var fstream;
+	if (request.busboy){
+		request.pipe(request.busboy);
+		request.busboy.on('file', function(fieldname, file, filename){
+			var newFilename = randomstring.generate(10);
+			fstream = fs.createWriteStream(__dirname + "/temp/" + newFilename);
+			file.pipe(fstream);
+			fstream.on('close', function(){
+				response.end(JSON.stringify({"success":newFilename}));
+				startStrukturJob(newFilename);
+			});
+		});
+	}
+});
+
+bilder.post("/prod", jsonParser, function(request, response){
+	var data = request.body;
+	getStructures();
+	if (data["prod"] && allStructures.indexOf(data["prod"]) != -1){
+		writeMetaJson(data["prod"]);
+	} else {
+		response.statusCode = 404;
+	}
+	response.end();
+});
+
+bilder.get("/axx", function(request, response){
+	if(request.query.name && getStructures().indexOf(request.query.name) != -1){
+		var cnt = fs.readdirSync("bilderStrukturen/_"+request.query.name + "/ChorservBilderStruktur/");
+		cnt = cnt.filter(function(val){
+			return fs.statSync(__dirname + config.struktur.bilder + "/_" + request.query.name + "/ChorservBilderStruktur/" + val).isDirectory()
+		});
+		response.end(JSON.stringify({content: cnt}));
+	} else {
+		response.statusCode = 404;
+		response.end();
+	}
+});
+
+function alnum(str){
+	//return str.match(/^[0-9a-z]+$/);
+	return true;
+}
+
+bilder.post("/change", jsonParser, function(request, response){
+	var data = request.body;
+	getStructures();
+	if (allStructures.indexOf(data.name) != -1){
+		if (data["rm"]){
+			for(var i = 0; i<data["rm"].length; i++){
+				/*if (!alnum(el)){
+					console.warn("mall: " + el);
+					return;
+				}*/
+				console.log("del " + data.name + "nd " + data["rm"][i]);
+				child_process.spawnSync('rm', ['-r', "bilderStrukturen/_"+data.name+"/ChorservBilderStruktur/"+data["rm"][i]]);
+			}
+		}
+		if (data["mrg"]){
+			if (!alnum(data["mrg"])) {
+				console.warn("mall: " + data["mrg"]);
+				return;
+			}
+			child_process.spawnSync('./mvt.sh', ['bilderStrukturen/_'+data.name+'/ChorservBilderStruktur', 'bilderStrukturen/_'+data["mrg"]+'/ChorservBilderStruktur/']);
+
+		}
+		console.log("Changes applied, starting job.");
+		if(data.rm){
+			littleJob(data.name)
+		} else {
+			littleJob(data.mrg);
+		}
+		response.end("ok");
+
+	} else {
+		response.statusCode = 404;
+	}
+});
+
+bilder.post("/commit", jsonParser, function (request, response){
+    // post param: transaction id, name, prod
+    var data = request.body;
+    var item =  job_collection[data["transaction_id"]];
+    if (item){
+        // commit
+        if (data["name"].indexOf("/") != -1 || data["name"].indexOf(".") != -1){
+            response.end(JSON.stringify({err: "Malformed Name"}));
+            return;
+        }
+        try{
+	        fs.rename("temp/"+item["stage30"]["file"]+".json", "bilderStrukturen/"+data["name"]+".json");    
+	        fs.rename("temp/"+item["stage30"]["directory"], "bilderStrukturen/"+data["name"]);
+	        child_process.spawn('mv', [item["stage10"].extracteddir, "bilderStrukturen/_"+data["name"]]);
+        } catch(err) {
+        	response.end(JSON.stringify({err: "Fatal FileSystem Error"}));
+        	return;
+        }
+        if (data["prod"]){
+            readMetaJson();
+            //TODO insecure
+            writeMetaJson(data["name"]);
+        }
+        response.end(JSON.stringify({name: data.name}));
+
+
+    }
+});
+
+var job_collection = {};
+bilder.get("/jobs", function (request, response){
+	response.end(JSON.stringify(job_collection));
+});
+
+function littleJob(extrdir, cb){
+	// _dir modified, rebuild dir and reinstatiate symlink
+	// dir without trailing thing is meant
+	jobname = extrdir + "_rebuild"
+	job_collection[jobname] = {}
+	job_collection[jobname].end = 0;
+
+	//del orig folder
+	if (extrdir.indexOf("/") != -1) return;
+	child_process.spawnSync("rm", ["-r", "bilderStrukturen/" + extrdir]);
+	var outF = "bilderStrukturen/" + extrdir + ".json"
+	var outD = "bilderStrukturen/" + extrdir;
+	var extracteddir = "bilderStrukturen/_" + extrdir;
+	fs.mkdirSync(outD);
+	var strukturlog = fs.createWriteStream('./struktur_job_out', {flags: 'w'});
+	var struktur = child_process.spawn(__dirname + "/" + config.requiredapps.struktur + "/struktur.py",
+		["-f", outF, "-w", outD, extracteddir + config.struktur.bilderDirectoryName]);	
+	struktur.stdout.pipe(strukturlog);
+	console.log(struktur);
+	struktur.on('close', (code) => {
+		job_collection[jobname].end = 1;
+		if (cb) cb();
+	});
+
+}
+
+function startStrukturJob(file, extrdir){
+	function humanVerification(file, json, directory){
+		job_collection[file].currentstage = 30;
+		job_collection[file]["stage30"] = {file: json, directory: directory};
+	}
+	function verifyAndRunStruktur(file, extracteddir){
+		/* stage 20 begin */
+		job_collection[file].currentstage = 20;
+		job_collection[file]["stage20"] = {};
+
+		var _utF = randomstring.generate(12);
+		var outF = __dirname + "/temp/" + _utF + ".json"; 
+
+		var _utD = randomstring.generate(13);
+		var outD = __dirname + "/temp/" + _utD;
+
+		fs.mkdirSync(outD);
+		var strukturlog = fs.createWriteStream('./struktur_job_out', {flags: 'w'});
+		var struktur = child_process.spawn(__dirname + "/" + config.requiredapps.struktur + "/struktur.py",
+			["-f", outF, "-w", outD, extracteddir + config.struktur.bilderDirectoryName]);	
+		struktur.stdout.pipe(strukturlog);
+		console.log(struktur);
+		struktur.on('close', (code) => {
+			job_collection[file]["stage20"].exit = code;
+			if(code == 0){
+				// Nur randomstring segmente in job_collection laden, um path disclosure zu vermeiden....
+				humanVerification(file, _utF, _utD);
+			}
+			console.log(job_collection);
+		});
+
+	}
+	job_collection[file] = {};
+	job_collection[file].currentstage = 10;
+	var dir = __dirname + "/temp/" + randomstring.generate(11);
+	const szip = child_process.spawn(config.requiredapps.p7zip, ['x', __dirname + "/temp/" + file, '-o' + dir]);
+	szip.on('close', (code) => {
+
+		/* stage 10 begin */
+		job_collection[file]["stage10"] = {};
+		job_collection[file]["stage10"].exit = code;
+		job_collection[file]["stage10"].extracteddir = dir;
+
+		if (code == 0){
+			verifyAndRunStruktur(file, dir);
+		}
+		console.log(job_collection);
+	});
+}
+
+
+backend.use("/kalender", kalender);
+backend.use("/newsticker", newsticker);
+backend.use("/bilder", bilder);
+backend.use("/static", express.static(__dirname + "/static"));
+backend.get(["/", "/index.html"], function(request, response){
+	response.sendFile("index.html", {root : __dirname});
+})
+
+var server = backend.listen(config.server.port, config.server.host, function(){
+	var address = server.address();
+	console.log("Backend gestartet auf: http://%s:%s", address.address, address.port);
+});
---
+++ static/navigation.js
@@ -0,0 +1,386 @@
+angular.module("backend", [])
+	.controller("NavigationController", function(){
+		var nav = this;
+
+		nav.CALENDAR = 0x01;
+		nav.BILDER = 0x02;
+		nav.NEWSTICKER = 0x03;
+		nav.TEXTE = 0x04;
+		nav.SERVERHEALTH = 0x05;
+		nav.BACKGROUND = 0x06;
+		nav.IDE = 0x07;
+		nav.DEFAULT = 0x00;
+
+		nav.selectedElement = nav.DEFAULT;
+
+		nav.activate = function(id){
+			nav.selectedElement = id;
+			if (nav.selectedElement != nav.DEFAULT){
+				nav.showNav = false;
+			} else {
+				nav.showNav = true;
+			}
+		};
+
+		nav.isActive = function(el){
+			return el == nav.selectedElement;
+		}
+
+		nav.showNav = true;
+		nav.showNavigation = function(){
+			return nav.showNav;
+		}
+	})
+	.controller("KalenderController", function($scope, $http){
+		$scope.entryDatabase = {"events":[]};
+
+		$scope.newItem = {
+			date: undefined,
+			time: undefined,
+			title: "",
+			loc: "",
+		}
+
+		$scope.neuerTermin = false;
+		$scope.toggleNeuerTermin = function(){
+			$scope.neuerTermin = !$scope.neuerTermin;
+		}
+
+		$scope.entries = function(){
+			return $scope.entryDatabase["events"].length;
+		}
+
+		$scope.getEntryDatabase = function(){
+			return $scope.entryDatabase["events"];
+		}
+
+		$scope.isBeforeNow = function(momentDate){
+			return momentDate.isBefore(moment());
+		}
+
+		$scope.allInputsValid = function(){
+			for (var i = 0; i<=3; i++){
+				if($scope.validateInput(i)) return false;
+			}
+			return true;
+		}
+
+		$scope.validateInput = function(d){
+			switch(d){
+				case 0: return $scope.newItemForm.date.$error.required || $scope.newItemForm.date.$error.date
+				case 1: return $scope.newItemForm.time.$error.required || $scope.newItemForm.time.$error.time
+				case 2: return $scope.newItemForm.title.$error.required
+				case 3: return $scope.newItemForm.loc.$error.required
+			}
+		}
+
+		$scope.add = function(){
+			var localOperateOn = $scope.entryDatabase;
+			var newItem = {
+				date: moment($scope.newItem.date),
+				location: $scope.newItem.loc,
+				time: moment($scope.newItem.time).format("HH:mm"),
+				title: $scope.newItem.title
+			};
+			/*localOperateOn["events"].push(newItem);
+			localOperateOn["events"].sort(compareMilliseconds);*/
+
+			var transmitter = newItem;
+			transmitter.date = transmitter.date.format("DD.MM.YYYY");
+			console.log("Übermittle:");
+			console.log(JSON.stringify(transmitter));
+			$http({
+				method: "POST",
+				url: "/kalender/add",
+				data: JSON.stringify(transmitter)
+			}).success(function(data){
+				$scope.performKalenderQuery();
+				$scope.newItem = {};
+			});
+
+			console.log($scope.entryDatabase);
+		}
+
+
+
+		var compareMilliseconds = function(a, b){
+			if(a.date.unix() > b.date.unix()) return -1;
+			if(a.date.unix() < b.date.unix()) return 1;
+			return 0;
+		}
+
+		$scope.rm = function(element){
+			var copy = JSON.parse(JSON.stringify(element));
+			copy.date = moment(copy.date).format("DD.MM.YYYY")
+			var request = {
+				type: "byObjType",
+				object: copy
+			}
+			console.log("Removing: " + JSON.stringify(request));
+			$http({
+				method: "POST",
+				url: "/kalender/rm",
+				data: JSON.stringify(request)
+			}).success(function(data){
+				console.log(data);
+				$scope.performKalenderQuery();
+			});
+			
+		}
+
+
+		$scope.performKalenderQuery = function(){
+			$http({
+				method: "GET",
+				url: "/kalender/get"
+			}).success(function(data){
+				for(var i = 0; i<data["events"].length; i++){
+					data["events"][i].date = moment(data["events"][i].date, "DD.MM.YYYY");
+				}
+				data["events"].sort(compareMilliseconds);
+				$scope.entryDatabase = data;
+
+			})
+		}
+		$scope.performKalenderQuery();
+	})
+	.controller("NewstickerController", function($scope, $http){
+		$scope.ticker = [];
+		$scope.getNewsticker = function(){
+			return $scope.ticker;
+		}
+		$scope.addNews = function(){
+			$scope.ticker.push("neuer ticker");
+		}
+		$scope.saveNews = function(){
+			alert(JSON.stringify($scope.ticker));
+			$http({
+				method: "POST",
+				url : "/newsticker/set",
+				data: JSON.stringify({text:$scope.ticker})
+			}).success(function(data){
+				alert(data);
+			});
+		}
+		$scope.rm = function(idx){
+			$scope.ticker.splice(idx, 1);
+		}
+		$http({
+			method: "GET",
+			url : "/newsticker/get"
+		}).success(function(data){
+			$scope.ticker = data["news"];
+		});
+	})
+	.service('fileUpload', ['$http', function ($http) {
+	    this.uploadFileToUrl = function(file, uploadUrl, success, failure){
+	    	if(success === undefined) success = function(){};
+	    	if(failure === undefined) failure = function(){};
+	        var fd = new FormData();
+	        fd.append('file', file);
+	        $http.post(uploadUrl, fd, {
+	            transformRequest: angular.identity,
+	            headers: {'Content-Type': undefined}
+	        })
+	        .success(success).error(failure);
+	    }
+	}])
+	.controller("IDEController", function($scope, $http){
+		var myCodeMirror = CodeMirror.fromTextArea(document.getElementById("codemirror"), {
+			mode: "javascript",
+			value : "function ohai(){};\n",
+			theme: "solarized"
+		});
+	})
+	.controller("IMGController", function($scope, $http, fileUpload){
+		$scope.bildStrukturen = [];
+		$scope.meta = {}
+		$scope.pollInterval = null;
+		$scope.currentStage = null;
+		$scope.commitTransaction = false;
+		$scope.tSaveAs = "";
+		$scope.axxL = {};
+
+		$scope.showDialog = false;
+		$scope.dcall = null;
+		$scope._dialogOK = function(){
+			$scope.dcall();
+			$scope.showDialog = false;
+		}
+		$scope.axx = function(structName){
+			if ($scope.hasaxx(structName)){
+				 delete $scope.axxL[structName];
+				 return;
+			}
+			$http.get("/bilder/axx?name=" + structName).success(function(data){
+				$scope.axxL[structName] = data.content;
+			}).error(function(error){
+				console.warn(error);
+			});
+		}
+		$scope.transactionDialog = function(title, desc, onok){
+			$scope.dialogTitle = title;
+			$scope.dialogBody = desc;
+			$scope.dcall = onok;
+			$scope.showDialog = true;
+		}
+		$scope.axxclass = function(structName){
+			if($scope.hasaxx(structName)) return "height:initial;";
+		}
+		$scope.hasaxx = function(structName){
+			return $scope.axxL.hasOwnProperty(structName);
+		}
+		$scope.axxList = function(structName){
+			return $scope.axxL[structName];
+		}
+		$scope.dialogCommitTransaction = function(){
+			if($scope.tSaveAs == "") return;
+			$scope.setTransaction("commit " + $scope.transactionName)
+			$http({
+				method: "POST",
+				url: "/bilder/commit",
+				data: JSON.stringify({
+					"transaction_id" : $scope.transactionName,
+					"prod" : $scope.tProd,
+					"name" : $scope.tSaveAs
+				})
+			})
+			.success(function(data){
+				if (data.err){
+					$scope.setTransaction("commit failed: " + data.err);
+					console.warn(data);
+					return;
+				}
+				$scope.setTransaction("commit ok ("+ data.name +")");
+				$scope.commitTransaction = false;
+				$scope.reload();
+			})
+			.error(function(data){
+				console.warn(data);
+				$scope.setTransaction("commit failed");	
+			});
+
+		}
+		$scope.deltransaction = function(structName, dirInStruct){
+			$scope.transactionDialog("TRANSACTION delete " + structName + " @ " + dirInStruct,
+				"Das Segment wird gelöscht und Serverseitig neu gebaut. OK?", function(){
+					$scope.setTransaction("del trans run");
+					$http.post("/bilder/change", JSON.stringify({
+						name: structName,
+						rm: [dirInStruct]
+					})).success(function(s){
+						$scope.setTransaction("del trans job..");
+						$scope.pollState();
+					}).error(function(e){
+						$scope.setTransaction("del fail");
+						console.warn(e);
+					});
+				})
+		}
+		$scope.mrg = function(structName){
+			$scope.transactionDialog("TRANSACTION merge " + structName + " nach " + $scope.meta.prod,
+				"Dieses Element wird auf Production gemerged. Existierende Daten werden überschrieben. Struktur wird neu gebaut. OK?", function(){
+					$scope.setTransaction("merge into prod");
+					$http.post("/bilder/change", JSON.stringify({
+						name: structName,
+						mrg: $scope.meta.prod
+					})).success(function(s){
+						$scope.setTransaction("merging..");
+						$scope.pollState();
+					}).error(function(e){
+						console.warn(e);
+						$scope.setTransaction("merge failure");
+					});
+				})
+		}
+		$scope.pollState = function(){
+			$scope.pollInterval = setInterval(function(){
+				$http({
+					method: "GET",
+					url: "bilder/jobs"
+				}).success(function(data){
+					var keys = Object.keys(data);
+					var job = data[keys[keys.length - 1]];
+					$scope.transactionName = keys[keys.length - 1];
+					$scope.tSaveAs = keys[keys.length -1];
+						if (job["end"] !== undefined){
+							if(job["end"] == 1){
+								$scope.setTransaction("Merge/Delete OK");
+								$scope.reload();
+								clearInterval($scope.pollInterval);
+								return;
+							}
+						} else {
+							$scope.currentStage = job["currentstage"];
+							$scope.setTransaction("at transaction: " + $scope.currentStage);
+							if(job["stage" + $scope.currentStage]){
+								if (job["stage" + $scope.currentStage]["exit"] != 0 && job["stage" + $scope.currentStage]["exit"] != undefined){
+									$scope.setTransaction("fatal transaction error: "+ job["stage" + $scope.currentStage]["exit"]);
+									clearInterval($scope.pollInterval);
+								}
+							}
+							if (job["stage30"]){
+								clearInterval($scope.pollInterval);
+								$scope.commitTransaction = true;
+							}
+						}
+				});
+			}, 1300);	
+		}
+		$scope.reload = function(){
+			$http({
+				method: "GET",
+				url: "/bilder/strukturen"
+			}).success(function(data){
+				$scope.bildStrukturen = data["strukturen"];
+				$scope.meta = data["meta"];
+				$scope.axxL = {};
+			});
+			// TODO default error handler
+			// TODO HTTP DEBUG INFORMATION
+		}	
+		$scope.submit = function(){
+			var f = document.getElementById('file').files[0];
+			$scope.setTransaction("Upload..");	
+			fileUpload.uploadFileToUrl(f, "/bilder/upload", function(suc){
+				$scope.setTransaction("Upload Complete.");	
+				$scope.pollState();
+			}, function(fail){
+				$scope.setTransaction("Upload failed.");
+			});
+
+			if (f === null){
+				alert("no file duh");
+			} 
+		}
+		$scope.setProd = function(structName){
+			$scope.setTransaction("Change Prod -> " + structName);
+			$http({
+				method: "POST",
+				url: "/bilder/prod",
+				data: JSON.stringify({"prod" : structName})
+			}).success(function(){
+				$scope.setTransaction("Prod Change OK")
+				$scope.reload();
+			}).error(function(){
+				$scope.setTransaction("transaction failed (404)")
+			});
+		}
+		$scope.getBildStrukturen = function(){
+			return $scope.bildStrukturen;
+		}
+		$scope.isProd = function(structName){
+			return structName === $scope.meta["prod"]? "img-btn-active" : "img-btn-inactive"
+		}
+		$scope.isNotProd = function(structName){
+			return structName !== $scope.meta["prod"]? "img-btn-active" : "img-btn-inactive"
+		}
+		$scope.setTransaction = function(text){
+			document.getElementById("transaction_status").innerHTML = text;
+			$scope.addConsoleLine(text);
+		}
+		$scope.addConsoleLine = function(line){
+			var e =document.getElementById("console");
+			e.innerHTML = line + "<br/>" + e.innerHTML;
+		}
+	});
